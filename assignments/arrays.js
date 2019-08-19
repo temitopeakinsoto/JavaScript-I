f482cdadb7e9bbc9a7f561858ec7287324963070
Temitope Akinsoto <takinsoto@gmail.com>
2019-08-19 14:04:53 +0100

    REFACTOR: Replace all var keywords with Let keywords:
    In this commit, i refactor my code and replace all var key words used withing the for loops. The var key words were all replaced with the let keywords for proper variable declaration/block scoping

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -76,7 +76,7 @@ let inventory = [
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
 let carWithIdThreeThree = '';
-for (var i = 0; i < inventory.length; i++){
+for (let i = 0; i < inventory.length; i++){
   if (inventory[i].id === 33)carWithIdThreeThree = inventory[i];
 }
 //console.log(carWithIdThreeThree);
@@ -93,7 +93,7 @@ console.log(`The last Car in the inventory is a ${carWithIdThreeThree.car_make} 
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
 let carModelsSorted = [];
-for (var i=0; i < inventory.length; i++){
+for (let i=0; i < inventory.length; i++){
   carModels.push(inventory[i].car_model);
 }
 carModelsSorted = carModels.sort();
@@ -106,7 +106,7 @@ console.log(carModelsSorted);
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
 let numOfCar = inventory.length;
-for (var i=0; i < numOfCar-1; i++){
+for (let i=0; i < numOfCar-1; i++){
   carYears.push(inventory[i].car_year);
 }
 console.log(carYears);
@@ -114,7 +114,7 @@ console.log(carYears);
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-for (var i = 0; i < carYears.length; i++){
+for (let i = 0; i < carYears.length; i++){
   if(carYears[i] < 2000) oldCars.push(carYears[i]);
 }
 console.log(oldCars.length);
@@ -122,7 +122,7 @@ console.log(oldCars.length);
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
-for (var i=0; i < inventory.length; i++){
+for (let i=0; i < inventory.length; i++){
   if(inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') BMWAndAudi.push(inventory[i]);
 }
 console.log(JSON.stringify(BMWAndAudi));
