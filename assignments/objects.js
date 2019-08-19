c3eaedf3394dfb31f21364b0db3c9b611b56ad58
Temitope Akinsoto <takinsoto@gmail.com>
2019-08-19 14:30:44 +0100

    Nested Object and the this keyword
    Create parent object with properties name and age. i made name susan and age 70. also nested a child object with name and age property as well. I set the name of the child object to George and its age to 50. Further creatd a grandchild object with name and age properties and set the name to Sam and age to 30.
    The three objects: Parent, child and grandchild objects were then given the speak method which basically outputs a greeting 'hello, my name is [respective name of the object]'

--- assignments/objects.js
+++ assignments/objects.js
@@ -96,7 +96,34 @@ antonietta.multiplyNums = function(num1, num2){
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name: "Susan",
+  age: 70,
+  child: {
+    name: "George",
+    age: 50,
+    grandchild: {
+      name: "Sam",
+      age: 30
+    }
+  }
+}
+
+parent.speak = function(){
+  console.log(`Hello, my name is ${this.name}`);
+}
+
+parent.child.speak = function(){
+  console.log(`Hello, my name is ${this.name}`);
+}
+
+parent.child.grandchild.speak = function(){
+  console.log(`Hello, my name is ${this.name}`);
+}
+
+parent.speak();
+parent.child.speak();
+parent.child.grandchild.speak();
 
 // Log the parent object's name
 
