804e944cc090b629690e761437fc561bf195378f
Temitope Akinsoto <takinsoto@gmail.com>
2019-08-19 13:14:52 +0100

    Display a list of  all car models alphabetically
    In this commit, i listed out all the models of the cars in the car dealership inventory and arranged them alphabetically. First, i got all the the car models in the inventory, i then returned an alphabetically sorted version of the results, at the request of the marketing department

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -93,7 +93,14 @@ console.log(`The last Car in the inventory is a ${carWithIdThreeThree.car_make} 
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
 let carModelsSorted = [];
-console.log();
+for (var i=0; i < inventory.length; i++){
+  carModels.push(inventory[i].car_model);
+}
+carModelsSorted = carModels.sort();
+console.log(carModelsSorted);
+// for(var i=0; i < carModelsSorted.length; i++){
+//   console.log( carModelsSorted[i]);
+// }
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
